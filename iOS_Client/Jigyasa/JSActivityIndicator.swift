80884522be3fd8be3f52d9f6d0f5b5a98575a60b
Basagond <basagond.mugganavar@cognitiveclouds.com>
2017-04-20 15:55:52 +0530

    Dev testing

--- iOS_Client/Jigyasa/JSActivityIndicator.swift
+++ iOS_Client/Jigyasa/JSActivityIndicator.swift
@@ -14,6 +14,7 @@ class JSActivityIndicator: UIView {
     
     override init(frame: CGRect) {
         
+        print("dev")
         super.init(frame:frame)
         self.backgroundColor = UIColor().activityIndicatorBackgroundColor
         
